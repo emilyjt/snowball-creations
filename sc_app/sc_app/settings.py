1b2d1f266dd825d1db550f2b706d7dba0ebc274b
maximussinclair <mrmaxsinclair@gmail.com>
2020-06-11 17:50:42 +0100

    Updated profile to get rid of profile pic for now. Also updated settings page with static root and static url and media root etc. all corrected for heroku

--- sc_app/sc_app/settings.py
+++ sc_app/sc_app/settings.py
@@ -121,13 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = 'sc_app/static'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'sc_app/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRIPSY_TEMPLATE_PACK = 'bootstrap4'
