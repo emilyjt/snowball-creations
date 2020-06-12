fcee30bcd13b2ece2b673dc8a4175c87ab8230ca
maximussinclair <mrmaxsinclair@gmail.com>
2020-06-12 09:51:28 +0100

    Added django-heroku back in as needed it seems

--- sc_app/sc_app/settings.py
+++ sc_app/sc_app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import django_heroku
 import os
 
 email_user = os.environ.get('EMAIL_USER')
@@ -144,3 +145,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'email_user'
 EMAIL_HOST_PASSWORD = 'email_pass'
+
+django_heroku.settings(locals())
