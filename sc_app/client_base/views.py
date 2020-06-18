c1006aba4e56c1d9803179514278f06f4e222648
maximussinclair <mrmaxsinclair@gmail.com>
2020-06-18 19:12:54 +0100

    Added urls to clientbase

--- sc_app/client_base/views.py
+++ sc_app/client_base/views.py
@@ -4,9 +4,8 @@ from django.db import models
 from dashboard.models import User, SocialNetwork, SocialProfile, Service, Subscription, Company
 from users.models import UserProfile
 
+
 # Create your views here.
-
-
 class ClientListView(ListView):
     template_name = 'client_base/client_list.html'
     queryset = Company.objects.all()
