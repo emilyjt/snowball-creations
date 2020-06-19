e77da61aa859ce4830e7debe909287d2b8f3e920
maximussinclair <mrmaxsinclair@gmail.com>
2020-06-19 20:18:13 +0100

    updated created at date for company

--- sc_app/dashboard/models.py
+++ sc_app/dashboard/models.py
@@ -1,6 +1,8 @@
 from djmoney.models.fields import MoneyField
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -64,7 +66,7 @@ class Company(models.Model):
     tertiary_contact = models.ForeignKey(User,  related_name='user_tertiary_contact', null=True, blank=True, on_delete=models.CASCADE)
     subscriptions = models.ManyToManyField(Subscription, blank=True)
     source = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    date_created = models.DateTimeField(_('date created'), default=timezone.now)
 
     # class Meta:
     #     ordering = ['name']
