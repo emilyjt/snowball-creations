ef454ed2f713fa6938b524fa333a98c5c418922f
maximussinclair <mrmaxsinclair@gmail.com>
2020-06-23 06:56:48 +0100

    Took mo's total price and added total number of clients to context too and then styled it in to top of page

--- sc_app/client_base/views.py
+++ sc_app/client_base/views.py
@@ -10,15 +10,22 @@ class ClientListView(ListView):
     model = Subscription
     template_name = 'client_base/client_list.html'
     queryset = Subscription.objects.all()
-    
+
     # Defines an alternate context to return (other than the standard queryset)
     def get_context_data(self, *args, **kwargs):
-        context = super(ClientListView, self).get_context_data(*args, **kwargs) 
+        context = super(ClientListView, self).get_context_data(*args, **kwargs)
         # Set an initial price of 0, iterate through each subscription and add the price
         # the existing price (there's probably a better way to do this, but I was short on time)
         price = 0
         for sub in Subscription.objects.all():
             price+=sub.service_used.price
         # Call this context using the name 'totalPrice'
-        context['totalPrice'] = price
+        shortprice = str(price)[2:]
+
+        context['totalPrice'] = shortprice
+
+        # Additional context for how many total clients currently.
+        
+        context['totalNumber'] = len(Subscription.objects.all())
+
         return context
